7a4a3c41e4f4997f9a88a72181fcc0549d0385b8
EustachyGra <123370214+EustachyGra@users.noreply.github.com>
2025-02-26 12:56:17 +0100

    Create graf.cpp

---
+++ graf.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+
+using namespace std;
+struct kraw{
+    int v1;
+    int v2;
+    int w;
+    
+    edge(int t1, int t2, int tw=1)
+    {
+        v1=t1;
+        v2=t2;
+        w=tw;
+    }
+
+};
+
+
+int main()
+{
+    
+}
